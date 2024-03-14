86dbea8bfbf0ed45d6a5e00e28a9e58c2059b518
griler <>
2024-03-15 01:34:27 +0700

    +add Login Scene

---
+++ Assets/Scripts/Login/LoginForm.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Login;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class LoginForm : MonoBehaviour
+{
+    [SerializeField] private GameObject username;
+    [SerializeField] private GameObject password;
+    [SerializeField] private Button btnLogin;
+
+    private string userName = "";
+    private string passWord = "";
+
+    private void Awake()
+    {
+        btnLogin.onClick.AddListener(clickLogin);
+    }
+
+    void clickLogin()
+    {
+        userName = username.GetComponent<TMP_InputField>().text;
+        passWord = password.GetComponent<TMP_InputField>().text;
+        userName = "thienloc123";
+        passWord = "12345";
+        if (userName != "" || passWord != "")
+        {
+            StartCoroutine(CallLogin(userName, passWord));
+        }
+    }
+
+    public IEnumerator CallLogin(string userName, string passWord)
+    {
+        User user = new User(userName, passWord);
+        var json = user.SaveToString();
+        string url = "localhost:3000/api/auth/login";
+        var bytes = Encoding.UTF8.GetBytes(json);
+        using (UnityWebRequest www = UnityWebRequest.Post(url, json, "application/json"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                var jsonResult = Encoding.Default.GetString(www.downloadHandler.data);
+                var test = user.fromToJson(jsonResult);
+                Debug.Log("Form upload complete! " + test.token);
+            }
+        }
+    }
+}
--- Assets/Scripts/Login/User.cs
+++ Assets/Scripts/Login/User.cs
@@ -1,7 +1,28 @@
-﻿namespace Login
+﻿using UnityEngine;
+
+namespace Login
 {
     public class User
     {
+        public string username;
+        public string password;
+        public string token;
+
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string SaveToString()
+        {
+            return JsonUtility.ToJson(this);
+        }
+
+        public User fromToJson(string json)
+        {
+            return JsonUtility.FromJson<User>(json);
+        }
         
     }
 }
